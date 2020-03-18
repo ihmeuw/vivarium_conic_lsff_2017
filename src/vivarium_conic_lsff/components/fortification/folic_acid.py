2ee7afcc14070b7b01ac68e4cdf030c04fd569ea
James Collins <collijk@uw.edu>
2020-03-18 13:17:08 -0700

    Need to know if individuals are effectively covered

--- src/vivarium_conic_lsff/components/fortification/folic_acid.py
+++ src/vivarium_conic_lsff/components/fortification/folic_acid.py
@@ -25,6 +25,24 @@ class FolicAcidFortificationCoverage:
             source=lambda: coverage_data
         )
 
+        self._column = 'mother_ate_folic_acid_fortified_food'
+        self.randomness = builder.randomness.get_stream(self._column)
+        self.population_view = builder.population.get_view([self._column])
+        builder.population.initializes_simulants(self.on_initialize_simulants,
+                                                 creates_columns=[self._column],
+                                                 requires_values=['folic_acid_fortification.effective_coverage_level'],
+                                                 requires_streams=[self._column])
+
+    def on_initialize_simulants(self, pop_data: 'SimulantData'):
+        if pop_data.user_data['sim_state'] == 'setup':  # Initial population
+            pop_update = pd.Series('unknown', index=pop_data.index, name=self._column)
+        else:  # New sims
+            draw = self.randomness.get_draw(pop_data.index)
+            effective_coverage = self.effective_coverage_level(pop_data.index)
+            pop_update = pd.Series((draw < effective_coverage).map({True: 'true', False: 'false'}),
+                                   index=pop_data.index)
+        self.population_view.update(pop_update)
+
     @staticmethod
     def load_coverage_data(builder: 'Builder'):
         location = builder.configuration.input_data.location
