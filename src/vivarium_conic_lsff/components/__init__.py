982e65745f2e645b10ccde6505a1e99ef6e9a027
Kjell Swedin <kjells@uw.edu>
2020-03-26 13:03:23 -0700

    refactor the folic acid fortification component to handle iron fortification as well

--- src/vivarium_conic_lsff/components/__init__.py
+++ src/vivarium_conic_lsff/components/__init__.py
@@ -1,7 +1,7 @@
 from .lbwsg import LBWSGRisk, LBWSGRiskEffect
 from .mortality import Mortality
 from .disease import VitaminADeficiency, IronDeficiency, NeonatalSWC_without_incidence
-from .fortification import (FolicAcidFortificationCoverage, FolicAcidFortificationEffect,
+from .fortification import (FolicAcidAndIronFortificationCoverage, FolicAcidFortificationEffect,
                             VitaminAFortificationCoverage, VitaminAFortificationEffect,
                             FortificationIntervention)
 from .observers import (DiseaseObserver, LiveBirthWithNTDObserver, LBWSGObserver,
