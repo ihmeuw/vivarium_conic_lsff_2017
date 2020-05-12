39462b17321cfa2bfc37c213d8830669879562f9
Kjell Swedin <kjells@uw.edu>
2020-05-12 15:07:12 -0700

    Add neonatal lri DiseaseModel using birth prevalence

--- src/vivarium_conic_lsff/components/disease/__init__.py
+++ src/vivarium_conic_lsff/components/disease/__init__.py
@@ -1,3 +1,4 @@
 from .iron_deficiency import IronDeficiency
 from .vitamin_a_deficiency import VitaminADeficiency
 from .neural_tube_defects import NeonatalSWC_without_incidence
+from .lri_birth_prevalence import NeonatalSIS
