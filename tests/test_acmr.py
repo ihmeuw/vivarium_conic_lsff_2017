29d2a1a891d63550cf0b78b5f79ce7db97511d21
Abraham Flaxman <abie@uw.edu>
2020-06-16 13:57:35 -0700

    BUG: use mortality_rate pipeline, not all_cause.mortality_hazard pipeline

--- tests/test_acmr.py
+++ tests/test_acmr.py
@@ -12,14 +12,15 @@ def test_acmr():
     pop = sim.get_population()
     age_groups = pd.cut(pop.age, bins=[0,7/365, 28/365, 1, 5], right=False)
 
-    acmr_pipeline = sim.get_value('all_causes.mortality_hazard')
+    mr_pipeline = sim.get_value('mortality_rate')
 
-    acmr_orig = acmr_pipeline.source(pop.index)
-    acmr_w_risk = acmr_pipeline(pop.index)
+    acmr_orig = mr_pipeline.source(pop.index).sum(axis=1)
+    acmr_w_risk = mr_pipeline(pop.index).sum(axis=1) * 365 # convert back to "per person-year"
+
     # confirm that they are *not* identical at the individual level
     assert not np.allclose(acmr_orig,
                            acmr_w_risk, rtol=.05), 'expect acmr to be quite different for some individuals'
 
     # but close at pop level
     assert np.allclose(acmr_orig.groupby(age_groups).median(),
-                       acmr_w_risk.groupby(age_groups).median(), rtol=.05), 'expect acmr to be within 5% of original at population level'
+                       acmr_w_risk.groupby(age_groups).median(), rtol=.1), 'expect acmr to be within 10% of original at population level'
