183f54fe2e97c257f42d0e070debca719a71b860
Kjell Swedin <kjells@uw.edu>
2021-01-13 11:50:41 -0800

    unpin components, exclude LBWSG data from artifact

--- setup.py
+++ setup.py
@@ -17,24 +17,24 @@ if __name__ == "__main__":
         long_description = f.read()
 
     install_requirements = [
-        'vivarium==0.9.3',
-        'vivarium_public_health==0.10.4',
+        'vivarium==0.10.1',
+        'vivarium_public_health==0.10.8',
 
         'click',
-        'gbd_mapping==2.1.0',
+        'gbd_mapping>=3.0.0, <4.0.0',
         'jinja2',
         'loguru',
-        'numpy<=1.15.4',
-        'pandas<0.25',
+        'numpy',
+        'pandas',
         'scipy',
-        'tables<=3.4.0',
-        'pyyaml'
+        'tables',
+        'pyyaml',
     ]
 
     # use "pip install -e .[dev]" to install required components + extra components
     extras_require = [
-        'vivarium_cluster_tools==1.2.1',
-        'vivarium_inputs[data]==3.1.1',
+        'vivarium_cluster_tools==1.2.3',
+        'vivarium_inputs[data]==4.0.1',
     ]
 
     setup(
@@ -62,8 +62,8 @@ if __name__ == "__main__":
 
         entry_points='''
             [console_scripts]
-            make_specs=vivarium_conic_lsff.tools.cli:make_specs
             make_artifacts=vivarium_conic_lsff.tools.cli:make_artifacts
             make_results=vivarium_conic_lsff.tools.cli:make_results
+            make_specs=vivarium_conic_lsff.tools.cli:make_specs
         '''
     )
